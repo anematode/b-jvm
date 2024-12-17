2ff0a6ab5940601101a09a895f88456e1da91408
Timothy Herchen <timothy.herchen@gmail.com>
2024-12-17 09:21:30 -0800

    link bootstrap methods to indy cp entries

--- src/bjvm.h
+++ src/bjvm.h
@@ -315,7 +315,7 @@ typedef struct {
   bjvm_bootstrap_method *method; // TODO convert to pointer
   bjvm_cp_name_and_type *name_and_type;
   bjvm_method_descriptor *method_descriptor;
-} bjvm_cp_invoke_dynamic_info;
+} bjvm_cp_indy_info;
 
 typedef enum {
   BJVM_CP_KIND_INVALID = 0,
@@ -354,7 +354,7 @@ typedef struct bjvm_cp_entry {
     bjvm_cp_method_info methodref;
     bjvm_cp_method_handle_info method_handle_info;
     bjvm_cp_method_type_info method_type_info;
-    bjvm_cp_invoke_dynamic_info indy_info;
+    bjvm_cp_indy_info indy_info;
   };
 } bjvm_cp_entry;
 
