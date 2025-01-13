4cffdb4e6b1b9349b7ef75100f54ac9eee96140f
alec <chop@quenda.net>
2025-01-13 15:19:01 -0800

    add jdk23

---
+++ src/classloader.h
@@ -0,0 +1,8 @@
+//
+// Created by alec on 1/13/25.
+//
+
+#ifndef BJVM_CLASSLOADER_H
+#define BJVM_CLASSLOADER_H
+
+#endif //BJVM_CLASSLOADER_H
---
+++ src/modules.h
@@ -0,0 +1,8 @@
+//
+// Created by alec on 1/13/25.
+//
+
+#ifndef BJVM_MODULES_H
+#define BJVM_MODULES_H
+
+#endif //BJVM_MODULES_H
