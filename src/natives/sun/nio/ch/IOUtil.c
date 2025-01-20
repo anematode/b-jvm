00ba3b8a8f7caf20708d74aeaabe155ddf642fb0
Timothy Herchen <timothy.herchen@gmail.com>
2025-01-19 23:49:22 -0800

    just stop

--- src/natives/sun/nio/ch/IOUtil.c
+++ src/natives/sun/nio/ch/IOUtil.c
@@ -7,9 +7,9 @@ DECLARE_NATIVE("sun/nio/ch", IOUtil, initIDs, "()V") {
 }
 
 DECLARE_NATIVE("sun/nio/ch", IOUtil, iovMax, "()I") {
-  return (bjvm_stack_value){.i = IOV_MAX};
+  return (bjvm_stack_value){.i = 1024};
 }
 
 DECLARE_NATIVE("sun/nio/ch", IOUtil, writevMax, "()J") {
-  return (bjvm_stack_value){.l = IOV_MAX};
+  return (bjvm_stack_value){.l = 1024};
 }
