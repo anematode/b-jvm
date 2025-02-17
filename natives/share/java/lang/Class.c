b15517f806fb2252935052a6b1b97129a55d6e03
Timothy Herchen <timothy.herchen@gmail.com>
2025-02-16 18:49:55 -0800

    maybe?

--- natives/share/java/lang/Class.c
+++ natives/share/java/lang/Class.c
@@ -169,7 +169,9 @@ DECLARE_NATIVE("java/lang", Class, forName0,
 
   if (c && args[1].i) {
     initialize_class_t ctx = {.args = {thread, c}};
+    thread->synchronous_depth++;
     future_t f = initialize_class(&ctx);
+    thread->synchronous_depth--;
     CHECK(f.status == FUTURE_READY);
 
     if (ctx._result) {
