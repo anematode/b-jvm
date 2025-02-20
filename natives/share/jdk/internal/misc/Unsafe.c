2fd1f5f26eda3263233e303b2801b25d86110661
Timothy Herchen <timothy.herchen@gmail.com>
2025-02-20 11:09:45 -0800

    ok

--- natives/share/jdk/internal/misc/Unsafe.c
+++ natives/share/jdk/internal/misc/Unsafe.c
@@ -204,6 +204,19 @@ DECLARE_NATIVE_OVERLOADED("jdk/internal/misc", Unsafe, putShort, "(Ljava/lang/Ob
   return value_null();
 }
 
+
+DECLARE_NATIVE_OVERLOADED("jdk/internal/misc", Unsafe, putDouble, "(JD)V", 1) {
+  DCHECK(argc == 3);
+  memcpy((char *)args[0].l, &args[1].d, sizeof(double));
+  return value_null();
+}
+
+DECLARE_NATIVE_OVERLOADED("jdk/internal/misc", Unsafe, putDouble, "(Ljava/lang/Object;JD)V", 2) {
+  DCHECK(argc == 3);
+  memcpy((char *)args[0].handle->obj + args[1].l, &args[2].d, sizeof(double));
+  return value_null();
+}
+
 DECLARE_NATIVE("jdk/internal/misc", Unsafe, putByte, "(Ljava/lang/Object;JB)V") {
   DCHECK(argc == 3);
   *(s8 *)((uintptr_t)args[0].handle->obj + args[1].l) = args[2].i;
