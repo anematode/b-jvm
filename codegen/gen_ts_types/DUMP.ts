3b44b06be345c1a8486e0d379f1ad3f92d3edd93
Dylan Pizzo <dylan@dylanpizzo.email>
2025-03-31 22:09:20 -0700

    move some stuff

--- codegen/gen_ts_types/DUMP.ts
+++
@@ -1,7 +0,0 @@
-declare class com_example_Simple {
-	new(): com_example_Simple;
-	simpleMethod(): void
-	[_BRAND]: {
-		Simple: true
-	};
-}
--- codegen/gen_ts_types/stuff.ts
+++
@@ -1,3 +0,0 @@
-const _BRAND = Symbol("brand");
-
-type Nominal<N extends string, T> = T & { [_BRAND]: Record<N, true> };
