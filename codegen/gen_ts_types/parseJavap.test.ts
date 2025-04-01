4450dc2aee59fff1dab1405834fbee3acaa75ac4
Dylan Pizzo <dylan@dylanpizzo.email>
2025-03-31 20:23:21 -0700

    add another test case

--- codegen/gen_ts_types/parseJavap.test.ts
+++ codegen/gen_ts_types/parseJavap.test.ts
@@ -186,5 +186,279 @@ describe("parseJavap", () => {
 				},
 			]);
 		});
+
+		it("should parse a class that extends another class", () => {
+			const javap = dedent`
+				Compiled from "Child.java"
+				public class com.example.Child extends com.example.Parent {
+					public com.example.Child();
+					public void childMethod();
+					protected int parentOverride(java.lang.String param);
+				}
+			`;
+
+			expectOutput(javap, [
+				{
+					className: "Child",
+					modifiers: ["public"],
+					interfaces: [],
+					superClass: "com.example.Parent",
+					methods: [
+						{
+							name: "Child",
+							kind: "constructor",
+							returnType: {
+								kind: "class",
+								name: "com.example.Child",
+							},
+							modifiers: ["public"],
+							parameters: [],
+							descriptor: "",
+						},
+						{
+							name: "childMethod",
+							kind: "method",
+							returnType: {
+								kind: "primitive",
+								type: "void",
+							},
+							modifiers: ["public"],
+							parameters: [],
+							descriptor: "",
+						},
+						{
+							name: "parentOverride",
+							kind: "method",
+							returnType: {
+								kind: "primitive",
+								type: "int",
+							},
+							modifiers: ["protected"],
+							parameters: [
+								{
+									name: "param",
+									type: {
+										kind: "class",
+										name: "java.lang.String",
+									},
+								},
+							],
+							descriptor: "",
+						},
+					],
+					fields: [],
+					isInterface: false,
+					packageName: "com.example",
+				},
+			]);
+		});
+
+		it("should parse a class that implements an interface", () => {
+			const javap = dedent`
+				Compiled from "Implementation.java"
+				public class com.example.Implementation implements com.example.SomeInterface {
+					public com.example.Implementation();
+					public void interfaceMethod(int value);
+					public java.lang.String getDescription();
+				}
+			`;
+
+			expectOutput(javap, [
+				{
+					className: "Implementation",
+					modifiers: ["public"],
+					interfaces: ["com.example.SomeInterface"],
+					methods: [
+						{
+							name: "Implementation",
+							kind: "constructor",
+							returnType: {
+								kind: "class",
+								name: "com.example.Implementation",
+							},
+							modifiers: ["public"],
+							parameters: [],
+							descriptor: "",
+						},
+						{
+							name: "interfaceMethod",
+							kind: "method",
+							returnType: {
+								kind: "primitive",
+								type: "void",
+							},
+							modifiers: ["public"],
+							parameters: [
+								{
+									name: "value",
+									type: { kind: "primitive", type: "int" },
+								},
+							],
+							descriptor: "",
+						},
+						{
+							name: "getDescription",
+							kind: "method",
+							returnType: {
+								kind: "class",
+								name: "java.lang.String",
+							},
+							modifiers: ["public"],
+							parameters: [],
+							descriptor: "",
+						},
+					],
+					fields: [],
+					isInterface: false,
+					packageName: "com.example",
+				},
+			]);
+		});
+
+		it("should parse a class that both extends a class and implements multiple interfaces", () => {
+			const javap = dedent`
+				Compiled from "FullExample.java"
+				public class com.example.FullExample extends com.example.BaseClass implements com.example.Interface1, com.example.Interface2, java.io.Serializable {
+					private static final long serialVersionUID;
+					protected com.example.Helper helper;
+					public com.example.FullExample(com.example.Helper helper);
+					public void interface1Method();
+					public java.lang.String interface2Method(int count);
+					protected com.example.Result baseClassOverride(java.lang.String input, int flags);
+					private void internalProcessing();
+					public static com.example.FullExample create(com.example.Helper helper);
+				}
+			`;
+
+			expectOutput(javap, [
+				{
+					className: "FullExample",
+					modifiers: ["public"],
+					interfaces: [
+						"com.example.Interface1",
+						"com.example.Interface2",
+						"java.io.Serializable",
+					],
+					superClass: "com.example.BaseClass",
+					methods: [
+						{
+							name: "FullExample",
+							kind: "constructor",
+							returnType: {
+								kind: "class",
+								name: "com.example.FullExample",
+							},
+							modifiers: ["public"],
+							parameters: [
+								{
+									name: "helper",
+									type: {
+										kind: "class",
+										name: "com.example.Helper",
+									},
+								},
+							],
+							descriptor: "",
+						},
+						{
+							name: "interface1Method",
+							kind: "method",
+							returnType: {
+								kind: "primitive",
+								type: "void",
+							},
+							modifiers: ["public"],
+							parameters: [],
+							descriptor: "",
+						},
+						{
+							name: "interface2Method",
+							kind: "method",
+							returnType: {
+								kind: "class",
+								name: "java.lang.String",
+							},
+							modifiers: ["public"],
+							parameters: [
+								{
+									name: "count",
+									type: { kind: "primitive", type: "int" },
+								},
+							],
+							descriptor: "",
+						},
+						{
+							name: "baseClassOverride",
+							kind: "method",
+							returnType: {
+								kind: "class",
+								name: "com.example.Result",
+							},
+							modifiers: ["protected"],
+							parameters: [
+								{
+									name: "input",
+									type: {
+										kind: "class",
+										name: "java.lang.String",
+									},
+								},
+								{
+									name: "flags",
+									type: { kind: "primitive", type: "int" },
+								},
+							],
+							descriptor: "",
+						},
+						{
+							name: "internalProcessing",
+							kind: "method",
+							returnType: {
+								kind: "primitive",
+								type: "void",
+							},
+							modifiers: ["private"],
+							parameters: [],
+							descriptor: "",
+						},
+						{
+							name: "create",
+							kind: "method",
+							returnType: {
+								kind: "class",
+								name: "com.example.FullExample",
+							},
+							modifiers: ["public", "static"],
+							parameters: [
+								{
+									name: "helper",
+									type: {
+										kind: "class",
+										name: "com.example.Helper",
+									},
+								},
+							],
+							descriptor: "",
+						},
+					],
+					fields: [
+						{
+							name: "serialVersionUID",
+							modifiers: ["private", "static", "final"],
+							type: { kind: "primitive", type: "long" },
+							descriptor: "",
+						},
+						{
+							name: "helper",
+							modifiers: ["protected"],
+							type: { kind: "class", name: "com.example.Helper" },
+							descriptor: "",
+						},
+					],
+					isInterface: false,
+					packageName: "com.example",
+				},
+			]);
+		});
 	});
 });
