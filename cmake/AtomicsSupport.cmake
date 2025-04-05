5f02e861dab85f5c503a5b29b64fe5f544cfb8f4
Max Cai <maxcai.paloalto@gmail.com>
2025-04-04 20:11:24 -0700

    move it to a special cmake file that is included, and adds to bjvm_static instead of everywhere

---
+++ cmake/AtomicsSupport.cmake
@@ -0,0 +1,33 @@
+include(CheckSourceCompiles)
+include(CheckLibraryExists)
+
+function(CHECK_WORKING_ATOMICS varname)
+    CHECK_SOURCE_COMPILES(C "
+        #include <stdatomic.h>
+        #include <stdint.h>
+
+        #define SIZEOF_POINTER (UINTPTR_MAX / 255 % 255)
+        #if SIZEOF_POINTER == 4
+        typedef uint64_t extended_data_t;
+        #else
+        typedef __uint128_t extended_data_t;
+        #endif
+
+        extended_data_t x; // sized to fit two void pointers
+
+        int main() {
+            extended_data_t i = __atomic_load_n(&x, __ATOMIC_SEQ_CST);
+            return 0;
+        }
+        " ${varname})
+endfunction(CHECK_WORKING_ATOMICS)
+
+message("Pointer size: ${CMAKE_SIZEOF_VOID_P} bytes")
+CHECK_WORKING_ATOMICS(HAVE_ATOMICS_WITHOUT_LIB)
+
+if (HAVE_ATOMICS_WITHOUT_LIB)
+    message("atomics already supported by gcc")
+else ()
+    message("adding link library atomic")
+    target_link_libraries(bjvm_static PRIVATE atomic)
+endif ()
